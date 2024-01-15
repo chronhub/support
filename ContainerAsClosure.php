b5a4e141eafa4afeb30c74fe020665e3a850993f
steph bug <chronhubgit@gmail.com>
2024-01-15 09:54:51 +0100

    wip

--- ContainerAsClosure.php
+++ ContainerAsClosure.php
@@ -6,13 +6,27 @@ namespace Storm\Support;
 
 use Closure;
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Foundation\Application;
 
 final readonly class ContainerAsClosure
 {
-    public Container $container;
+    /**
+     * @deprecated
+     */
+    public Application $container;
 
     public function __construct(Closure $container)
     {
         $this->container = $container();
+    }
+
+    public function getApplication(): Application
+    {
+        return $this->container;
+    }
+
+    public function getContainer(): Container
+    {
+        return $this->container;
     }
 }
