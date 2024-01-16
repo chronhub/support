2f70d8a05c099b75395bb23f3336bc7f975f125a
steph bug <chronhubgit@gmail.com>
2024-01-16 10:19:26 +0100

    wip(reporter)

---
+++ Message/MessageDecoratorSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Storm\Support\Message;
+
+use Carbon\Carbon;
+use Closure;
+use Storm\Contract\Message\Header;
+use Storm\Contract\Tracker\MessageStory;
+use Symfony\Component\Uid\Uuid;
+
+final class MessageDecoratorSubscriber
+{
+    public function __invoke(): Closure
+    {
+        return function (MessageStory $story): void {
+            $message = $story->message();
+
+            if ($message->hasNot(Header::EVENT_ID)) {
+                $message = $message->withHeader(Header::EVENT_ID, Uuid::v4()->jsonSerialize());
+            }
+
+            if ($message->hasNot(Header::EVENT_TIME)) {
+                $message = $message->withHeader(Header::EVENT_TIME, Carbon::now('UTC')->format('Y-m-d TH:i:s.u'));
+            }
+
+            if ($message->hasNot(Header::EVENT_TYPE)) {
+                $message = $message->withHeader(Header::EVENT_TYPE, $message->name());
+            }
+
+            if ($message->hasNot(Header::EVENT_DISPATCHED)) {
+                $message = $message->withHeader(Header::EVENT_DISPATCHED, false);
+            }
+
+            $story->withMessage($message);
+        };
+    }
+}
--- Providers/StormServiceProvider.php
+++ Providers/StormServiceProvider.php
@@ -5,16 +5,12 @@ declare(strict_types=1);
 namespace Storm\Support\Providers;
 
 use Illuminate\Support\AggregateServiceProvider;
-use Storm\Attribute\AttributeServiceProvider;
 use Storm\Message\MessageServiceProvider;
-use Storm\Reporter\ReporterServiceProvider;
 
 class StormServiceProvider extends AggregateServiceProvider
 {
     protected $providers = [
         GenericServiceProvider::class,
         MessageServiceProvider::class,
-        ReporterServiceProvider::class,
-        AttributeServiceProvider::class,
     ];
 }
