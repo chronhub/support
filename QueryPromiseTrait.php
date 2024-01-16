ba87019d0a48f0ff49baab26e9a1c568f8c4b849
steph bug <chronhubgit@gmail.com>
2024-01-16 11:05:03 +0100

    feat(reporter): impl generic event and query reporter

---
+++ QueryPromiseTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Storm\Support;
+
+use React\Promise\PromiseInterface;
+
+trait QueryPromiseTrait
+{
+    public function handle(PromiseInterface $promise, bool $raiseException = true): mixed
+    {
+        $result = null;
+        $exception = null;
+
+        $promise->then(
+            function ($value) use (&$result) {
+                $result = $value;
+            },
+            function ($reason) use (&$exception) {
+                $exception = $reason;
+            }
+        );
+
+        if ($exception && $raiseException) {
+            throw $exception;
+        }
+
+        return $result;
+    }
+}
